3cfcf047952b7c9e6d62e412289ce1259e35e156
Vishnu-prakashC <cvishnu6600@gmail.com>
2025-07-18 23:42:53 +0530

    day-2 with socket io

--- server/server.js
+++ server/server.js
@@ -1,11 +1,39 @@
 const express=require('express');
-const app=express();
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
 
-app.get('/',(req,res)=>{
-    res.send('backend successfully running');
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ['GET', 'POST']
+    }
 });
-const port=3000;
+app.use(cors());
+app.use(express.json());
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
+const PORT=5000;
+//const PORT = 5000;
+
+//ithu socket io connection
+
+io.on('connection', (socket) => {
+    console.log('A user connected:' + socket.id);
+
+    socket.on('send_message', (data) => {
+        io.emit('receive_message', data);
+    });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected' + socket.id);
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('backend successfully running......');
+});
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
